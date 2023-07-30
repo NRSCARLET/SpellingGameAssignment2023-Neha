d0bdb072f2a41f37ee5482d2189c094014161772
NRSCARLET <107818695+NRSCARLET@users.noreply.github.com>
2023-07-30 22:21:38 +0000

    I made a new file and started coding a user login for the user.

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ window = tk.Tk()
 window.title("Hello world")
 window.geometry("300x300")
 
-hello = tk.Label(text="Hello world!")
+hello = tk.Label(text="Welcome to spelling game")
 hello.pack()
-button = tk.Button(text="Click me!")
+button = tk.Button(text="User Login")
 button.pack()
-
+ 
 tk.mainloop()
---
+++ userlogin.py
@@ -0,0 +1,7 @@
+class Userlogin:
+    def __init__(self, name):
+        self.name = name
+
+def login():
+    username = input("What will your username be?")
+    
