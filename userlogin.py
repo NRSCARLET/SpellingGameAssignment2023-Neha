cacd9cf707f180e2bd02347ae1ce2a140cd00eba
NRSCARLET <107818695+NRSCARLET@users.noreply.github.com>
2023-08-01 00:22:24 +0000

    Trying to print text onto GUI instead of console. used code I found online. Makes a separate window pop up. Need to fix.

--- userlogin.py
+++ userlogin.py
@@ -1,7 +1,42 @@
-class Userlogin:
-    def __init__(self, name):
-        self.name = name
+import sys 
+import tkinter as tk 
+class PrintLogger: 
+    def __init__(self, textbox): 
+        self.textbox = textbox 
+ 
+    def write(self, text): 
+        self.textbox.insert(tk.END, text) 
+ 
+root = tk.Tk() 
+ 
+textbox = tk.Text(root) 
+textbox.pack() 
+ 
+printlogger = PrintLogger(textbox) 
+sys.stdout = printlogger 
+ 
+# print("Hello, world!")  # This will appear in the Text widgetroot.mainloop()
+
+window = tk.Tk()
+window.title("Button Speller")
+window.geometry("300x300")
+
+hello = tk.Label(text="Welcome to the spelling game!")
+hello.pack()
 
 def login():
-    username = input("What will your username be?")
+    print("Hello Tester")
+
+
+userbutton = tk.Button(text="User Login", 
+                   command=login)
+userbutton.pack()
+ 
+tk.mainloop()
+class Userlogin:
+    def __init__(self, name, highscore):
+        self.name = name
+        self.highscore = highscore
+
+
     
