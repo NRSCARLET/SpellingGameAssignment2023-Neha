73f41081804a39514c662547dc0d6439b7e2ed28
NRSCARLET <107818695+NRSCARLET@users.noreply.github.com>
2023-08-03 01:50:26 +0000

    I have successfully coded a print function onto the GUI when a button is pressed

---
+++ GUIprinttest.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import tkinter as tk
+def printGUI():
+    label = Label(text="Hello")
+    label.pack()
+window = tk.Tk()
+window.title("Hello world")
+window.geometry("300x300")
+
+hello = tk.Label(text="Welcome to the Spelling Bee Spelling Game")
+hello.pack()
+button = tk.Button(text="Print", command=printGUI)
+button.pack()
+ 
+tk.mainloop()
